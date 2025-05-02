a07030e586062e92cc665b5cb5698055cf161a78
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-05-01 22:18:38 -0400

    Update Supabase connection

--- Sources/SupachatModel/ViewModel.swift
+++ Sources/SupachatModel/ViewModel.swift
@@ -2,54 +2,7 @@ import Foundation
 import Observation
 import SkipFuse
 import SkipKeychain
-
-fileprivate let client = SupabaseClient(
-    supabaseURL: URL(string: "https://zncizygaxuzzvxnsfdvp.supabase.co")!,
-    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpuY2l6eWdheHV6enZ4bnNmZHZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4NjE1NDksImV4cCI6MjAyMzQzNzU0OX0.yoFteItT4FVu_kbMuMnQCzE8YYU5jEVWLU7NDBY94-E",
-    options: SupabaseClientOptions(auth: SupabaseClientOptions.AuthOptions(storage: SimpleAuthLocalStorage()))
-)
-
-// WIP
-
-/// An auth storage that uses the iOS Keychain and Android EncyptedSharedPreferences
-struct SimpleAuthLocalStorage : AuthLocalStorage {
-    func store(key: String, value: Data) throws {
-        logger.info("SimpleAuthLocalStorage: store key: \(key)")
-    }
-
-    func retrieve(key: String) throws -> Data? {
-        logger.info("SimpleAuthLocalStorage: retrieve key: \(key)")
-        return nil
-    }
-
-    func remove(key: String) throws {
-        logger.info("SimpleAuthLocalStorage: remove key: \(key)")
-    }
-}
-
-
-// WIP
-
-struct Country: Codable {
-    var id: Int
-    var name: String
-    var created: Date? = nil
-    var gdp: Decimal? = nil
-}
-
-func fetchCountries() async throws -> [Country] {
-    logger.log("running testSkipSupabase")
-
-    let countriesResp: PostgrestResponse<[Country]> = try await client
-      .from("countries")
-      .select()
-      .order("id")
-      .limit(1)
-      .execute(options: FetchOptions(head: false, count: CountOption.exact))
-    let countries = countriesResp.value
-    return countries
-}
-
+import Supabase
 
 /// A logger for the SupachatModel module.
 let logger: Logger = Logger(subsystem: "supachat.model", category: "SupachatModel")
@@ -143,3 +96,63 @@ extension ViewModel {
         }
     }
 }
+
+// MARK: Supabase Credentials
+
+// Update this section with your own Supabase account into
+
+fileprivate let supabaseURL = "https://zncizygaxuzzvxnsfdvp.supabase.co"
+fileprivate let supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpuY2l6eWdheHV6enZ4bnNmZHZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4NjE1NDksImV4cCI6MjAyMzQzNzU0OX0.yoFteItT4FVu_kbMuMnQCzE8YYU5jEVWLU7NDBY94-E"
+
+fileprivate let client = SupabaseClient(
+    supabaseURL: URL(string: supabaseURL)!,
+    supabaseKey: supabaseKey,
+    options: SupabaseClientOptions(auth: SupabaseClientOptions.AuthOptions(storage: SimpleAuthLocalStorage()))
+)
+
+/// An auth storage that uses the iOS Keychain and Android EncyptedSharedPreferences.
+///
+/// Data is stored in base64-encoded string values.
+struct SimpleAuthLocalStorage : AuthLocalStorage {
+    func store(key: String, value: Data) throws {
+        logger.info("SimpleAuthLocalStorage: store key: \(key)")
+        try Keychain.shared.set(value.base64EncodedString(), forKey: key)
+    }
+
+    func retrieve(key: String) throws -> Data? {
+        logger.info("SimpleAuthLocalStorage: retrieve key: \(key)")
+        guard let value = try Keychain.shared.string(forKey: key) else {
+            return nil
+        }
+        return Data(base64Encoded: value)
+    }
+
+    func remove(key: String) throws {
+        logger.info("SimpleAuthLocalStorage: remove key: \(key)")
+        try Keychain.shared.removeValue(forKey: key)
+    }
+}
+
+
+// WIP
+
+struct Country: Codable {
+    var id: Int
+    var name: String
+    var created: Date? = nil
+    var gdp: Decimal? = nil
+}
+
+func fetchCountries() async throws -> [Country] {
+    logger.log("running testSkipSupabase")
+
+    let countriesResp: PostgrestResponse<[Country]> = try await client
+        .from("countries")
+        .select()
+        .order("id")
+        .limit(1)
+        .execute(options: FetchOptions(head: false, count: CountOption.exact))
+    let countries = countriesResp.value
+    return countries
+}
+
